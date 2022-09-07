94069506b753a560b4cf0942c0b2cdc96ae4ccff
mithulix <mohaiminul.devs@gmail.com>
2022-09-07 09:47:30 +0600

    client-side

---
+++ src/Components/Login/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_EMfGIT2YAOiI4D6eYVlaaL5QGtR8cQ0",
+  authDomain: "travelhub-14cda.firebaseapp.com",
+  projectId: "travelhub-14cda",
+  storageBucket: "travelhub-14cda.appspot.com",
+  messagingSenderId: "332712109994",
+  appId: "1:332712109994:web:54188c961dab7737e8add3",
+  measurementId: "G-QVL1932VD4"
+};
+export default firebaseConfig;
